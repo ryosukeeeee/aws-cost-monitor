47027c7034b0a06d64a0a581c1426d1a20d35361
ryosukeeeee <ryou.fm.bros@gmail.com>
2019-06-19 06:40:58 +0900

    [Update] 日本語フォントに対応した

--- graph.js
+++ graph.js
@@ -1,6 +1,12 @@
 const { CanvasRenderService } = require('chartjs-node-canvas');
 // サンプルデータの読み込み
 const sampleOptions = require('./sample-data.json');
+const path = require("path");
+const Canvas = require('canvas');
+const fontName = process.env['FONT_NAME'];
+const fontFamily = process.env['FONT_FAMILY'];
+// デフォルトだと日本語が文字化けするので日本語フォントを登録する
+Canvas.registerFont(path.join(__dirname, `fonts/${fontName}`), { family: fontFamily });
 
 module.exports.sampleGraphPlot = () => {
   return new Promise(async (resolve, reject) => {
