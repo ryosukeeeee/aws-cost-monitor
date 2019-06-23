aadaaef1432a4c3122ab9d05b57bb8c2ddfa91e9
ryosukeeeee <ryou.fm.bros@gmail.com>
2019-06-23 19:47:53 +0900

    [Add] YYYY-MM-DD形式の日付を返す関数を追加

--- handler.js
+++ handler.js
@@ -48,3 +48,14 @@ module.exports.main = async (event) => {
     return error;
   }
 };
+
+const getFormattedDateBeforeParam = (daysAgo = 0) => {
+  const date = new Date();
+  date.setDate(date.getDate() - daysAgo);
+
+  const year = date.getFullYear() + '';
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+
+  return `${year}-${month}-${day}`;
+}
